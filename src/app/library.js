fa919f7dd66a9f9d48532933126353c0d54ff330
Alihamza8899 <ramzan8899005@gmail.com>
2025-08-31 20:53:05 +0500

    Product changes

--- src/app/library.js
+++ src/app/library.js
@@ -1,17 +1,35 @@
 let getCatagery = async () => {
-    const response = await fetch('https://fakestoreapi.in/api/products/category');
+  const response = await fetch('https://fakestoreapi.in/api/products/category');
+  if (!response.ok) {
+    // Avoid JSON.parse on bad response
+    throw new Error(`Failed to fetch products: ${res.statusText}`);
+  }
+  else {
     const data = await response.json();
-    return data.categories ;
+    return data.categories;
+  }
 }
 let getProducts = async () => {
-    const response = await fetch('https://fakestoreapi.in/api/products');
+  const response = await fetch('https://fakestoreapi.in/api/products');
+  if (!response.ok) {
+    // Avoid JSON.parse on bad response
+    throw new Error(`Failed to fetch products: ${res.statusText}`);
+  }
+  else {
     const data = await response.json();
-    return data.products 
+    return data.products
+  }
 }
 let getProductDetail = async (product_id = null) => {
-    const response = await fetch('https://fakestoreapi.in/api/products/' + product_id);
+  const response = await fetch('https://fakestoreapi.in/api/products/' + product_id);
+  if (!response.ok) {
+    // Avoid JSON.parse on bad response
+    throw new Error(`Failed to fetch products: ${response.statusText}`);
+  }
+  else {
     const data = await response.json();
     return data.product;
+  }
 }
 
-export {getCatagery , getProducts , getProductDetail}
+export { getCatagery, getProducts, getProductDetail }
--- src/component/home/recentlyAddProduct.jsx
+++ src/component/home/recentlyAddProduct.jsx
@@ -1,4 +1,4 @@
-"use client"
+ "use client"
 import React, { useEffect, useState } from 'react'
 import Container from './container.jsx'
 import Product from '../product.jsx'
